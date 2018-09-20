14a9cbc7bf7bb47ad37e84510fdd38711b656e87
lukasz.kujawa <lukasz.kujawa@argos.co.uk>
2018-09-20 18:51:27 +0100

    update

---
+++ src/test/java/com/test/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import static junit.framework.TestCase.assertTrue;
+
+
+public class ApplicationTest {
+
+    @Test
+    public void testSomething() {
+       assertTrue(true);
+    }
+
+}
